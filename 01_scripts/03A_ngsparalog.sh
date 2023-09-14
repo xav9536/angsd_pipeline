e7cfb8119df71f493ffdaebad759658aab880bd9
Xavier Dallaire <61600047+xav9536@users.noreply.github.com>
2023-09-14 13:29:15 -0400

    Update 03A_ngsparalog.sh

--- 01_scripts/03A_ngsparalog.sh
+++ 01_scripts/03A_ngsparalog.sh
@@ -28,7 +28,7 @@ MIN_IND_FLOAT=$(echo "($N_IND * $PERCENT_IND)"| bc -l)
 MIN_IND=${MIN_IND_FLOAT%.*} 
 MAX_DEPTH=$(echo "($N_IND * $MAX_DEPTH_FACTOR)" |bc -l)
 
-echo "Calculate the SAF, MAF and GL for all individuals listed in 02_info/bam.filelist"
+echo "Calculate the MAF and HWE for all individuals listed in 02_info/bam.filelist"
 echo "keep loci with at least $MIN_DEPTH read for n individuals = $MIN_IND, which is $PERCENT_IND % of total $N_IND individuals"
 echo "filter on allele frequency = $MIN_MAF"
 
