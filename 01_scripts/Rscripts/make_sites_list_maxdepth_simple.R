ab2228bde912f3d5d30296400370a7e6cd511b3f
Xavier Dallaire <61600047+xav9536@users.noreply.github.com>
2023-09-14 12:01:20 -0400

    Update make_sites_list_maxdepth_simple.R

--- 01_scripts/Rscripts/make_sites_list_maxdepth_simple.R
+++ 01_scripts/Rscripts/make_sites_list_maxdepth_simple.R
@@ -6,12 +6,11 @@
 argv <- commandArgs(T)
 INFILE <- argv[1]
 OUTFILE_sites <- argv[2]
-OUTFILE_regions <- argv[3]
 
 maf<-read.table(INFILE, header=T)
-head (maf)
+head(maf)
 sites<-maf[,1:4]
 sites_order <- sites[order(sites$chromo),] 
-regions_order<-levels(sites_order$chromo)
+
 write.table(sites_order, OUTFILE_sites, row.names=F, col.names=F, sep="\t", quote=F)
-write.table(regions_order, OUTFILE_regions, row.names=F, col.names=F, sep="\t", quote=F)
+
